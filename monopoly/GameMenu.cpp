d2b5df6ee14cb6b15cf4ffd59beee0f597b449f7
m1pmap <dok_koks@mail.ru>
2023-11-07 18:31:25 +0300

    1.1 Add more functional for Menu

--- monopoly/GameMenu.cpp
+++ monopoly/GameMenu.cpp
@@ -14,6 +14,9 @@ int main(array<String^>^ args)
 	Application::Run(% form1);
 }
 
+bool arrowBool = true;
+int currentImageIndex = 0;
+
 System::Void GameMenu::StartGame_Click(System::Object^ sender, System::EventArgs^ e)
 {
 	GameForm^ f = gcnew GameForm(this);
@@ -25,3 +28,103 @@ System::Void GameMenu::closeGame_Click(System::Object^ sender, System::EventArgs
 {
 	this->Close();
 }
+
+System::Void GameMenu::arrowRight_Click(System::Object^ sender, System::EventArgs^ e)
+{
+	arrowRight->Visible = false;		//скрытие всех компонентов 1-го меню
+	startGame->Visible = false;
+	timer1->Start();	//запуск таймера и анимации
+	arrowBool = false;
+}
+
+System::Void GameMenu::timer1_Tick(System::Object^ sender, System::EventArgs^ e)
+{
+	if (!arrowBool) //проверка булевой переменной(true - анимация влево, false - анимация вправо) 
+	{
+		if (card->Left > -1900)	//таймер и анимация действует и перемещается вправо до момента пока координаты не достигнут установленного предела
+			card->Left -= 50;
+		else {	//если достигнут предел
+			timer1->Stop();	//остановка таймера
+			arrowLeft->Visible = true; //показ кнопки, возвращения на 1-ое меню
+			this->BackgroundImage = Image::FromFile(Application::StartupPath + "\\assets\\bcg.png"); //смена свойства BackgroundImage у формы
+			//показ всех элементов 2-го меню
+			characters->Visible = true;
+			arrowNext->Visible = true;
+			arrowPrev->Visible = true;
+			UserName->Visible = true;
+			changeName->Visible = true;
+		}
+	}
+	else {
+		if (card->Left < 0)	//таймер и анимация действует и перемещается влево до момента пока координаты не достигнут установленного предела
+			card->Left += 50;
+		else {	//если достигнут предел
+			timer1->Stop();	//остановка таймера
+			arrowRight->Visible = true;	//показ кнопки, возвращения на 2-ое меню
+			this->BackgroundImage = Image::FromFile(Application::StartupPath + "\\assets\\menuBackground.png"); //смена свойства BackgroundImage у формы
+			//показ всех элементов 1-го меню
+			startGame->Visible = true;
+		}
+	}
+}
+
+System::Void GameMenu::arrowLeft_Click(System::Object^ sender, System::EventArgs^ e)
+{
+	arrowNext->Visible = false;  //скрытие всех компонентов 2-го меню
+	arrowPrev->Visible = false;
+	characters->Visible = false;
+	arrowLeft->Visible = false;
+	UserName->Visible = false;
+	changeName->Visible = false;
+	timer1->Start();	//запуск таймера и анимации
+	arrowBool = true;
+}
+
+System::Void GameMenu::arrowNext_Click(System::Object^ sender, System::EventArgs^ e)
+{
+	array<String^>^ images = gcnew array<String^>{ "character1.png", "character2.png", "character3.png", "character4.png"}; // массив картинок
+	currentImageIndex++;		//установка номера картинки(следующей)
+	if (currentImageIndex >= 4)	//всего в массиве 4 картинки, поэтому если ноиер равен 4, то значит стоит начать отсчёт сначала
+	{
+		currentImageIndex = 0;
+	}
+	characters->Image = Image::FromFile(Application::StartupPath + "\\assets\\" + images[currentImageIndex]); //установка картинки соответсвующего номера
+}
+
+System::Void GameMenu::arrowPrev_Click(System::Object^ sender, System::EventArgs^ e)
+{
+	array<String^>^ images = gcnew array<String^>{ "character1.png", "character2.png", "character3.png", "character4.png"}; // массив картинок
+	currentImageIndex--; //установка номера картинки(предыдущей)
+	if (currentImageIndex < 0) //всего в массиве 4 картинки, поэтому если ноиер равен меньше 0, то значит стоит начать отсчёт с конца
+	{
+		currentImageIndex = 3;
+	}
+	characters->Image = Image::FromFile(Application::StartupPath + "\\assets\\" + images[currentImageIndex]); //установка картинки соответсвующего номера
+}
+
+System::Void GameMenu::Unfocus(System::Object^ sender, System::EventArgs^ e)
+{
+	arrowPrev->Focus();
+}
+
+//кнопка изменения никкнейма игрока и её свойства:
+bool changeNameBool = true;
+
+System::Void GameMenu::changeName_Click(System::Object^ sender, System::EventArgs^ e)
+{
+	if (changeNameBool) // проверка булевой переменной(true - можно изменять имя игрока, false - сохранить изменения и запрет именять имя игрока)
+	{
+		UserName->ReadOnly = false; //разрешение изменять имя игрока
+		changeName->Image = Image::FromFile(Application::StartupPath + "\\assets\\save_onMouseUp.png"); //смена картинки на "сохранить"
+		changeNameBool = false; //изменение на противоположное значение
+	}
+	else
+	{
+		UserName->ReadOnly = true;	//запрет изменять имя игрока
+		changeName->Image = Image::FromFile(Application::StartupPath + "\\assets\\changeName_onMouseUp.png");	//смена картинки на "изменить"
+		changeNameBool = true;	//изменение на противоположное значение
+
+	}
+}
+
+
--- monopoly/GameMenu.h
+++ monopoly/GameMenu.h
@@ -34,8 +34,20 @@ namespace monopoly {
 				delete components;
 			}
 		}
-	public: System::Windows::Forms::PictureBox^ StartGame;
+	public: System::Windows::Forms::PictureBox^ startGame;
+	protected:
+
 	private: System::Windows::Forms::PictureBox^ closeGame;
+	private: System::Windows::Forms::PictureBox^ card;
+	private: System::Windows::Forms::PictureBox^ arrowRight;
+	private: System::Windows::Forms::PictureBox^ arrowLeft;
+	private: System::Windows::Forms::Timer^ timer1;
+	private: System::Windows::Forms::PictureBox^ characters;
+	private: System::Windows::Forms::PictureBox^ arrowPrev;
+	private: System::Windows::Forms::PictureBox^ arrowNext;
+	private: System::Windows::Forms::PictureBox^ changeName;
+	private: System::Windows::Forms::TextBox^ UserName;
+	private: System::ComponentModel::IContainer^ components;
 	public:
 	protected:
 
@@ -47,7 +59,7 @@ namespace monopoly {
 		/// <summary>
 		/// Обязательная переменная конструктора.
 		/// </summary>
-		System::ComponentModel::Container ^components;
+
 
 #pragma region Windows Form Designer generated code
 		/// <summary>
@@ -56,24 +68,41 @@ namespace monopoly {
 		/// </summary>
 		void InitializeComponent(void)
 		{
+			this->components = (gcnew System::ComponentModel::Container());
 			System::ComponentModel::ComponentResourceManager^ resources = (gcnew System::ComponentModel::ComponentResourceManager(GameMenu::typeid));
-			this->StartGame = (gcnew System::Windows::Forms::PictureBox());
+			this->startGame = (gcnew System::Windows::Forms::PictureBox());
 			this->closeGame = (gcnew System::Windows::Forms::PictureBox());
-			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->StartGame))->BeginInit();
+			this->card = (gcnew System::Windows::Forms::PictureBox());
+			this->arrowRight = (gcnew System::Windows::Forms::PictureBox());
+			this->arrowLeft = (gcnew System::Windows::Forms::PictureBox());
+			this->timer1 = (gcnew System::Windows::Forms::Timer(this->components));
+			this->characters = (gcnew System::Windows::Forms::PictureBox());
+			this->arrowPrev = (gcnew System::Windows::Forms::PictureBox());
+			this->arrowNext = (gcnew System::Windows::Forms::PictureBox());
+			this->changeName = (gcnew System::Windows::Forms::PictureBox());
+			this->UserName = (gcnew System::Windows::Forms::TextBox());
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->startGame))->BeginInit();
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->closeGame))->BeginInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->card))->BeginInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->arrowRight))->BeginInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->arrowLeft))->BeginInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->characters))->BeginInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->arrowPrev))->BeginInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->arrowNext))->BeginInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->changeName))->BeginInit();
 			this->SuspendLayout();
 			// 
-			// StartGame
+			// startGame
 			// 
-			this->StartGame->BackColor = System::Drawing::Color::Transparent;
-			this->StartGame->Image = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"StartGame.Image")));
-			this->StartGame->Location = System::Drawing::Point(715, 467);
-			this->StartGame->Name = L"StartGame";
-			this->StartGame->Size = System::Drawing::Size(515, 116);
-			this->StartGame->SizeMode = System::Windows::Forms::PictureBoxSizeMode::Zoom;
-			this->StartGame->TabIndex = 1;
-			this->StartGame->TabStop = false;
-			this->StartGame->Click += gcnew System::EventHandler(this, &GameMenu::StartGame_Click);
+			this->startGame->BackColor = System::Drawing::Color::Transparent;
+			this->startGame->Image = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"startGame.Image")));
+			this->startGame->Location = System::Drawing::Point(716, 469);
+			this->startGame->Name = L"startGame";
+			this->startGame->Size = System::Drawing::Size(515, 116);
+			this->startGame->SizeMode = System::Windows::Forms::PictureBoxSizeMode::Zoom;
+			this->startGame->TabIndex = 1;
+			this->startGame->TabStop = false;
+			this->startGame->Click += gcnew System::EventHandler(this, &GameMenu::StartGame_Click);
 			// 
 			// closeGame
 			// 
@@ -87,6 +116,116 @@ namespace monopoly {
 			this->closeGame->TabStop = false;
 			this->closeGame->Click += gcnew System::EventHandler(this, &GameMenu::closeGame_Click);
 			// 
+			// card
+			// 
+			this->card->BackColor = System::Drawing::Color::Transparent;
+			this->card->BackgroundImage = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"card.BackgroundImage")));
+			this->card->Image = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"card.Image")));
+			this->card->Location = System::Drawing::Point(0, 0);
+			this->card->Name = L"card";
+			this->card->Size = System::Drawing::Size(3840, 1080);
+			this->card->TabIndex = 3;
+			this->card->TabStop = false;
+			// 
+			// arrowRight
+			// 
+			this->arrowRight->BackColor = System::Drawing::Color::Transparent;
+			this->arrowRight->Image = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"arrowRight.Image")));
+			this->arrowRight->Location = System::Drawing::Point(1837, 646);
+			this->arrowRight->Name = L"arrowRight";
+			this->arrowRight->Size = System::Drawing::Size(55, 124);
+			this->arrowRight->TabIndex = 4;
+			this->arrowRight->TabStop = false;
+			this->arrowRight->Click += gcnew System::EventHandler(this, &GameMenu::arrowRight_Click);
+			// 
+			// arrowLeft
+			// 
+			this->arrowLeft->BackColor = System::Drawing::Color::Transparent;
+			this->arrowLeft->Image = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"arrowLeft.Image")));
+			this->arrowLeft->Location = System::Drawing::Point(30, 646);
+			this->arrowLeft->Name = L"arrowLeft";
+			this->arrowLeft->Size = System::Drawing::Size(55, 124);
+			this->arrowLeft->TabIndex = 5;
+			this->arrowLeft->TabStop = false;
+			this->arrowLeft->Visible = false;
+			this->arrowLeft->Click += gcnew System::EventHandler(this, &GameMenu::arrowLeft_Click);
+			// 
+			// timer1
+			// 
+			this->timer1->Interval = 10;
+			this->timer1->Tick += gcnew System::EventHandler(this, &GameMenu::timer1_Tick);
+			// 
+			// characters
+			// 
+			this->characters->BackColor = System::Drawing::Color::Transparent;
+			this->characters->BackgroundImageLayout = System::Windows::Forms::ImageLayout::None;
+			this->characters->Image = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"characters.Image")));
+			this->characters->Location = System::Drawing::Point(150, 152);
+			this->characters->Name = L"characters";
+			this->characters->Size = System::Drawing::Size(1696, 863);
+			this->characters->TabIndex = 6;
+			this->characters->TabStop = false;
+			this->characters->Visible = false;
+			// 
+			// arrowPrev
+			// 
+			this->arrowPrev->BackColor = System::Drawing::Color::Transparent;
+			this->arrowPrev->Image = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"arrowPrev.Image")));
+			this->arrowPrev->Location = System::Drawing::Point(1237, 469);
+			this->arrowPrev->Name = L"arrowPrev";
+			this->arrowPrev->Size = System::Drawing::Size(46, 68);
+			this->arrowPrev->TabIndex = 9;
+			this->arrowPrev->TabStop = false;
+			this->arrowPrev->Visible = false;
+			this->arrowPrev->Click += gcnew System::EventHandler(this, &GameMenu::arrowPrev_Click);
+			// 
+			// arrowNext
+			// 
+			this->arrowNext->BackColor = System::Drawing::Color::Transparent;
+			this->arrowNext->Image = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"arrowNext.Image")));
+			this->arrowNext->Location = System::Drawing::Point(1737, 469);
+			this->arrowNext->Name = L"arrowNext";
+			this->arrowNext->Size = System::Drawing::Size(46, 68);
+			this->arrowNext->TabIndex = 8;
+			this->arrowNext->TabStop = false;
+			this->arrowNext->Visible = false;
+			this->arrowNext->Click += gcnew System::EventHandler(this, &GameMenu::arrowNext_Click);
+			// 
+			// changeName
+			// 
+			this->changeName->BackColor = System::Drawing::Color::Transparent;
+			this->changeName->Image = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"changeName.Image")));
+			this->changeName->Location = System::Drawing::Point(384, 282);
+			this->changeName->Name = L"changeName";
+			this->changeName->Size = System::Drawing::Size(330, 71);
+			this->changeName->TabIndex = 11;
+			this->changeName->TabStop = false;
+			this->changeName->Visible = false;
+			this->changeName->Click += gcnew System::EventHandler(this, &GameMenu::changeName_Click);
+			// 
+			// UserName
+			// 
+			this->UserName->BackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(231)), static_cast<System::Int32>(static_cast<System::Byte>(159)),
+				static_cast<System::Int32>(static_cast<System::Byte>(115)));
+			this->UserName->BorderStyle = System::Windows::Forms::BorderStyle::None;
+			this->UserName->Font = (gcnew System::Drawing::Font(L"Impact", 68.54F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->UserName->ForeColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(50)), static_cast<System::Int32>(static_cast<System::Byte>(21)),
+				static_cast<System::Int32>(static_cast<System::Byte>(54)));
+			this->UserName->ImeMode = System::Windows::Forms::ImeMode::NoControl;
+			this->UserName->Location = System::Drawing::Point(384, 164);
+			this->UserName->MaxLength = 7;
+			this->UserName->Name = L"UserName";
+			this->UserName->ReadOnly = true;
+			this->UserName->ShortcutsEnabled = false;
+			this->UserName->Size = System::Drawing::Size(400, 112);
+			this->UserName->TabIndex = 10;
+			this->UserName->TabStop = false;
+			this->UserName->Text = L"YourName";
+			this->UserName->Visible = false;
+			this->UserName->WordWrap = false;
+			this->UserName->MouseLeave += gcnew System::EventHandler(this, &GameMenu::Unfocus);
+			// 
 			// GameMenu
 			// 
 			this->AccessibleRole = System::Windows::Forms::AccessibleRole::None;
@@ -98,20 +237,51 @@ namespace monopoly {
 			this->BackgroundImageLayout = System::Windows::Forms::ImageLayout::None;
 			this->ClientSize = System::Drawing::Size(1904, 1041);
 			this->ControlBox = false;
+			this->Controls->Add(this->changeName);
+			this->Controls->Add(this->UserName);
+			this->Controls->Add(this->arrowPrev);
+			this->Controls->Add(this->arrowNext);
+			this->Controls->Add(this->arrowLeft);
+			this->Controls->Add(this->arrowRight);
 			this->Controls->Add(this->closeGame);
-			this->Controls->Add(this->StartGame);
+			this->Controls->Add(this->startGame);
+			this->Controls->Add(this->characters);
+			this->Controls->Add(this->card);
 			this->DoubleBuffered = true;
 			this->FormBorderStyle = System::Windows::Forms::FormBorderStyle::None;
 			this->Name = L"GameMenu";
 			this->Text = L"GameMenu";
 			this->WindowState = System::Windows::Forms::FormWindowState::Maximized;
-			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->StartGame))->EndInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->startGame))->EndInit();
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->closeGame))->EndInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->card))->EndInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->arrowRight))->EndInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->arrowLeft))->EndInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->characters))->EndInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->arrowPrev))->EndInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->arrowNext))->EndInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->changeName))->EndInit();
 			this->ResumeLayout(false);
+			this->PerformLayout();
 
 		}
 #pragma endregion
-	private: System::Void StartGame_Click(System::Object^ sender, System::EventArgs^ e);
-	private: System::Void closeGame_Click(System::Object^ sender, System::EventArgs^ e);
-	};
+private: System::Void StartGame_Click(System::Object^ sender, System::EventArgs^ e);
+	
+private: System::Void closeGame_Click(System::Object^ sender, System::EventArgs^ e);
+	
+private: System::Void arrowRight_Click(System::Object^ sender, System::EventArgs^ e);
+	
+private: System::Void timer1_Tick(System::Object^ sender, System::EventArgs^ e);
+	
+private: System::Void arrowLeft_Click(System::Object^ sender, System::EventArgs^ e);
+	
+private: System::Void arrowNext_Click(System::Object^ sender, System::EventArgs^ e);
+	
+private: System::Void arrowPrev_Click(System::Object^ sender, System::EventArgs^ e);
+
+private: System::Void Unfocus(System::Object^ sender, System::EventArgs^ e);
+
+private: System::Void changeName_Click(System::Object^ sender, System::EventArgs^ e);
+};
 }
